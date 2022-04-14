c740c21ff5e11548d21a763341492b31e8bf7971
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-04-14 13:33:49 -0700

    add automatic password

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,4 +4,10 @@ class Employee < ActiveRecord::Base
   validates :last_name, presence: true
   validates :hourly_rate, :inclusion => 40..200
   validates :store, presence: true
+  validates :password, :string
+
+  before_create do
+    self.password = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
+    string = (0...50).map { o[rand(o.length)] }.join
+  end
 end
